6f217ee735eba72e0d8912c2118773c3ee3d0a62
Andrii V <kidstonek@gmail.com>
2022-08-20 20:56:58 +0300

    basic finish

--- client.py
+++ client.py
@@ -8,20 +8,16 @@ srv_connection = server_ip, server_port
 def clt():
     client_skt = socket.socket()
     client_skt.connect(srv_connection)
-    client_skt.send('YO!'.encode())
+    client_skt.send(str(input('Type your first message: ')).encode())
+    while True:
+        data = client_skt.recv(2052).decode()
+        if data:
+            print(f'You received message {data}')
+        if data == 'bye':
+            print('Server closed connection by kye work "bye"')
+            break
+        client_skt.send(str(input('Your message: ')).encode())
     client_skt.close()
-
-    # print(f"We got connection from {addrs}")
-    # while True:
-    #     data = conn.recv(2052).decode()
-    #     if not data:
-    #         break
-    #     print(f"Received message: {data}")
-    #     msg = input(">>> ")
-    #     if msg == "stop":
-    #         break
-    #     conn.send(msg.encode())
-    # conn.close()
 
 
 if __name__ == '__main__':
--- server.py
+++ server.py
@@ -1,25 +1,31 @@
 import socket
+import asyncio
 
 server_ip = socket.gethostname()
 server_port = 56300
 
 
 def srv():
+    print(f"Server started!!! on port {server_port}")
     sck = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sck.bind((server_ip, server_port))
     sck.listen(2)
     conn, addrs = sck.accept()
     print(f"We got connection from {addrs}")
-    while True:
-        data = conn.recv(2052).decode()
-        if not data:
-            break
-        print(f"Received message: {data}")
-        msg = input(">>> ")
-        if msg == "stop":
-            break
-        conn.send(msg.encode())
-    conn.close()
+    try:
+        while True:
+            data = conn.recv(2052).decode()
+            if not data:
+                break
+            print(f"Received message: {data} from client {addrs[0]}")
+            msg = input(">>> ")
+            if msg == "stop":
+                break
+            conn.send(msg.encode())
+    except KeyboardInterrupt:
+        print('Connection stopped')
+    finally:
+        conn.close()
 
 
 def main():
